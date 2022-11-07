f49504b8c505f36d246ebeb2b9345a2bb8f5927d
Jonas Søeborg Nielsen <jonassoeborgnielsen@gmail.com>
2022-11-07 16:17:17 +0100

    Add files via upload

---
+++ dataloader.py
@@ -0,0 +1,45 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import os
+
+
+class CarDataset(Dataset):
+    
+    def __init__(self, directory):
+        # Load data
+        
+        directory = "C:/Users/jonas/Downloads/data/clean_data/train_data"
+        
+        x = []
+        y = []
+        
+        for filename in os.listdir(directory):
+            if filename[-4:] == ".npy":
+                data = np.load(os.path.join(directory, filename))
+                x.append(data[:3])
+                y.append(data[3])
+
+        self.n_samples = len(x)
+        x = np.array(x)
+        y = np.array(y)
+        self.x = torch.from_numpy(x)
+        self.y = torch.from_numpy(y)
+                
+    
+    def __getitem__(self, index):
+        # dataset[]
+        return self.x[index], self.y[index]
+    
+    
+    def __len__(self):
+        # len(dataset)
+        return self.n_samples
+
+
+batchsize = 10
+
+dataset = CarDataset()
+dataloader = DataLoader(dataset=dataset, batch_size=batchsize, shuffle=True)
+
+dataiter = iter(dataloader)
