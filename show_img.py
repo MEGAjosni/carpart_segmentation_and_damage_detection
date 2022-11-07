5996921a7d01dd68df04c974b686b279b87f13c9
Sandoar <s194323@student.dtu.dk>
2022-11-07 13:24:49 +0100

    Update show_img.py

--- show_img.py
+++ show_img.py
@@ -2,36 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from time import sleep
-from PIL import Image
 
 
 folder = r"C:\Users\aleks\OneDrive\Skrivebord\clean_data\train_data\\"
 
-def numpy2pil(np_array: np.ndarray) -> Image:
-    """
-    Convert an HxWx3 numpy array into an RGB Image
-    """
-
-    assert_msg = 'Input shall be a HxWx3 ndarray'
-    assert isinstance(np_array, np.ndarray), assert_msg
-    assert len(np_array.shape) == 3, assert_msg
-    assert np_array.shape[2] == 3, assert_msg
-
-    img = Image.fromarray(np_array, 'RGB')
-    return img
-
 for filename in os.listdir(folder):
     arr = np.load(folder+filename)
-    arr = arr[0:3]
-    arr = np.transpose(arr, (1,2,0))
     
-    plt.imshow(arr.astype(np.uint8))
+    fig, axs = plt.subplots(2, 2,sharex='col', sharey='row',
+                        gridspec_kw={'hspace': 0, 'wspace': 0})
+    axs[0,0].imshow(arr[0])
+    axs[0,0].set_title('First Channel')
+    axs[0,1].imshow(arr[1])
+    axs[0,1].set_title('Second Channel')
+    axs[1,0].imshow(arr[2])
+    axs[1,0].set_title('Third Channel')
+    axs[1,1].imshow(arr[3])
+    axs[1,1].set_title('Fourth Channel')
+    
     plt.show()
     
-    #img = Image.fromarray(arr, 'RGB')
-    #img.show()
-    
-    break
-    sleep(2)
+    #break
+    #sleep(2)
     
     
