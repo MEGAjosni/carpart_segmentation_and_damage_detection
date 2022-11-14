4143448568760c0368e920a3f3d9a717e2ddf80c
Sandoar <s194323@student.dtu.dk>
2022-11-14 10:06:51 +0100

    clean up

--- training.py
+++ training.py
@@ -110,79 +110,6 @@ def train_NN(model, train_loader, test_loader, batch_size=64, num_epochs=20, val
                 print(f"             test loss: {loss.item()}")
     
     print("Finished training.")
-        
-    
-#%% Training with 2 losses
-
-def train_NN2(model, train_loader, test_loader, batch_size=64, num_epochs=20, validation_every_steps=500, learning_rate=0.001, loss_fn1=nn.BCEWithLogitsLoss(), loss_fn2 = DiceLoss()):
-
-    device = "cpu"
-    if torch.cuda.is_available():
-        device = "cuda:0"  
-        
-    print(device)
-        
-    model.to(device)
-
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)    
-
-    step = 0
-    model.train()
-    
-    for epoch in tqdm(range(num_epochs)):
-        
-        print("epoch :", epoch)
-        test_loss = []
-        train_loss = []
-        
-        for inputs, targets in tqdm(train_loader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            
-            # Forward pass, compute gradients, perform one training step.
-            output = model(inputs)
-            batch_loss1 = loss_fn1(output, targets)
-            batch_loss2 = loss_fn2(output, targets)
-            batch_loss = batch_loss1 + batch_loss2
-            optimizer.zero_grad()
-            batch_loss.backward()
-            optimizer.step()
-    
-            # Increment step counter
-            step += 1
-            print("step: ",step,"loss: ",batch_loss.item())
-            
-            train_loss.append(batch_loss.item())
-            
-            if step % validation_every_steps == 0:
-                
-                
-                # Compute accuracies on validation set.
-                with torch.no_grad():
-                    model.eval()
-                    for inputs, targets in test_loader:
-                        inputs, targets = inputs.to(device), targets.to(device)
-                        output = model(inputs)
-                        loss1 = loss_fn1(output, targets)
-                        loss2 = loss_fn2(output, targets)
-                        loss = loss1 + loss2
-                        
-                        test_loss.append(loss.item())
-        
-                    
-                    #plotfun(images,labels,1)
-                    #vae.cuda()
-        
-                    model.train()
-                    
-                # Append average validation accuracy to list.
-         
-                print(f"Step {step:<5}   training loss: {batch_loss.item()}")
-                print(f"             test loss: {loss.item()}")
-    
-    print("Finished training.")
-        
-    
-
  
     
 #%% define VAE
@@ -208,11 +135,7 @@ train_loader = DataLoader(dataset=train_set, batch_size=batchsize, shuffle=True)
 test_loader = DataLoader(dataset=test_set, batch_size=batchsize, shuffle=True)
 
 #%%
-train_NN(vae,train_loader,test_loader,batch_size=batchsize,validation_every_steps=25,loss_fn = DiceLoss(), learning_rate=0.002)
-
-#%%
-train_NN2(vae,train_loader,test_loader,batch_size=batchsize,validation_every_steps=25)
-
+train_NN(vae,train_loader,test_loader,batch_size=batchsize,validation_every_steps=25,loss_fn = DiceLoss(), learning_rate=0.001)
 
 #%%
 import matplotlib.pyplot as plt
