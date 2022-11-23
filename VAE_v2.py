correct dice calculation

VAE_v2.py
@@ -16,7 +16,7 @@ class VAE_v2(nn.Module):
         super(VAE_v2,self).__init__()
     
         self.relu = nn.ReLU()
-        self.softmax = torch.Softmax(dim=1)
+        self.softmax = torch.nn.Softmax(dim=1)
         #conv1
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=32)
         

dataloader.py
@@ -1,12 +1,11 @@
 
-#%%
 import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
-from torchvision.transforms import InterpolationMode
+import torch.nn.functional as F
 
 
 user = 'Marcus'
@@ -71,7 +70,7 @@ class CarDataset(Dataset):
             carpart = int(carpart)
             mask[carpart][label[0] == carpart] = 1.0
         return mask
-
+    
 augmentations = transforms.Compose([transforms.Resize(size = imagewidth),
                                     transforms.RandomHorizontalFlip(p=0.5),
                                     transforms.RandomRotation((-45,45)),
@@ -101,16 +100,16 @@ def plot_things(images,labels,predictions = [], idx = 0, carpart = all):
     fig, axs = plt.subplots(1,n, sharey='row',
                         gridspec_kw={'hspace': 0, 'wspace': 0})
     
-    axs[0].imshow(image.numpy())
+    axs[0].imshow(image.detach().cpu().numpy())
     axs[0].set_title('Image')
-    axs[1].imshow(label,cmap = "gray")
+    axs[1].imshow(label.detach().cpu().numpy(),cmap = "gray")
     axs[1].set_title('Ground truth')
     axs[0].set_axis_off()
     axs[1].set_axis_off()
     if n > 2:
         prediction = predictions[idx]
         prediction = torch.argmax(prediction,dim = 0) if carpart == all else prediction[carpart,:,:]
-        axs[2].imshow(prediction.detach().numpy(),cmap = "gray")
+        axs[2].imshow(prediction.detach().cpu().numpy(),cmap = "gray")
         axs[2].set_title('Prediction')
         axs[2].set_axis_off()
     plt.show()

training.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from pytorch_toolbelt.losses import dice
+from pytorch_toolbelt.losses.functional import soft_dice_score
 
 classes = torch.arange(0,8)
 
@@ -75,6 +76,7 @@ def train_NN(model, train_loader, val_loader, save_file='untitled', batch_size=6
             
             if step % validation_every_steps == 0:
                 
+                val_acc = []
                 train_loss.append(batch_loss.item())
                 # Compute accuracies on validation set.
                 with torch.no_grad():
@@ -83,14 +85,17 @@ def train_NN(model, train_loader, val_loader, save_file='untitled', batch_size=6
                         inputs, targets = inputs.to(device), targets.to(device)
                         output = model(inputs)
                         loss = loss_fn(output, targets.long())
+                        #accuracy
+                        y_pred = F.one_hot(torch.argmax(output, dim = 1),9)
+                        y_pred = y_pred.permute(0, 3, 1, 2)
+                        dice_score = soft_dice_score(y_pred,targets)
+                        val_acc.append(dice_score)
                         
                         val_loss.append(loss.item())
                     
                     idx = 0 #choose first element in batch
-                    carpart = 0 #show door
-                    plot_things(inputs,targets, prediction = output ,idx = idx, carpart = all)
-
-        
+                    print("dice score: ", val_acc[-1])
+                    plot_things(inputs,targets, output ,idx = idx, carpart = all)
                     model.train()
                     
                 # Append average validation accuracy to list.
@@ -140,12 +145,12 @@ val_loader = DataLoader(dataset=val_set, batch_size=batchsize, shuffle=True)
 import torchvision.transforms as transforms
 
 batchsize = 8
-unet = UNet(out_channels=8)
+unet = UNet(out_channels=9)
 unet.double()
 
 imagewidth = 128
 augmentations_train = transforms.Compose([transforms.Resize(size = imagewidth),
-                                    transforms.RandomRotation((0,180)),
+                                    transforms.RandomRotation((-30,30)),
                                     transforms.RandomHorizontalFlip(p=0.5),
                                     ])
 
@@ -159,7 +164,7 @@ train_loader = DataLoader(dataset=train_set, batch_size=batchsize, shuffle=True)
 val_loader = DataLoader(dataset=val_set, batch_size=batchsize, shuffle=True)
 
 #%%
-train_loss, val_loss = train_NN(model=unet,train_loader=train_loader,val_loader=val_loader,save_file='unet',batch_size=batchsize,validation_every_steps=100, 
+train_loss, val_loss = train_NN(model=unet,train_loader=train_loader,val_loader=val_loader,save_file='unet',batch_size=batchsize,validation_every_steps=1,
                                 learning_rate=0.005,num_epochs=20, loss_fn = dice.DiceLoss("multilabel",classes))
 
 #%% plot loss