9be481831a39bfd4184510637fe30e8e983bf7cb
MarcusGalea <s194336@student.dtu.dk>
2022-12-05 23:03:11 +0100

    UNET2 FIX

--- Unet_v2.py
+++ Unet_v2.py
@@ -18,7 +18,7 @@ if torch.cuda.is_available():
     
 print("Device:",device)
 
-""" This model is taken from: https://github.com/mateuszbuda/brain-segmentation-pytorch/blob/master/unet.py
+""" This model is taken from: https://github.com/mateuszbuda/brain-segmentation-pytorch/blob/master/UNet2.py
 
 Remember to reference!!!"""
 
@@ -36,47 +36,47 @@ class ScaleFunc(torch.autograd.Function):
     
 class ScaleLayer(nn.Module):
 
-    def __init__(self, init_value=1e-3):
-        super().__init__()
-        self.scale = nn.Parameter(torch.FloatTensor(1).fill_(init_value))
+   def __init__(self, init_value=1e-3):
+       super().__init__()
+       self.scale = nn.Parameter(torch.FloatTensor([init_value]))
 
-    def forward(self, input):
-        return ScaleFunc.apply(input, self.scale)
+   def forward(self, input):
+       return input * self.scale
     
-class UNet(nn.Module):
+class UNet2(nn.Module):
 
     def __init__(self, in_channels=3, out_channels=9, init_features=32):
-        super(UNet, self).__init__()
+        super(UNet2, self).__init__()
         self.softmax = torch.nn.Softmax(dim=1)
 
         features = init_features
-        self.encoder1 = UNet._block(self,in_channels, features, name="enc1")
+        self.encoder1 = UNet2._block(self,in_channels, features, name="enc1")
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder2 = UNet._block(self,features, features * 2, name="enc2")
+        self.encoder2 = UNet2._block(self,features, features * 2, name="enc2")
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder3 = UNet._block(self,features * 2, features * 4, name="enc3")
+        self.encoder3 = UNet2._block(self,features * 2, features * 4, name="enc3")
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder4 = UNet._block(self,features * 4, features * 8, name="enc4")
+        self.encoder4 = UNet2._block(self,features * 4, features * 8, name="enc4")
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.bottleneck = UNet._block(self,features * 8, features * 16, name="bottleneck")
+        self.bottleneck = UNet2._block(self,features * 8, features * 16, name="bottleneck")
 
         self.upconv4 = nn.ConvTranspose2d(
             features * 16, features * 8, kernel_size=2, stride=2
         )
-        self.decoder4 = UNet._block(self,(features * 8) * 2, features * 8, name="dec4")
+        self.decoder4 = UNet2._block(self,(features * 8) * 2, features * 8, name="dec4")
         self.upconv3 = nn.ConvTranspose2d(
             features * 8, features * 4, kernel_size=2, stride=2
         )
-        self.decoder3 = UNet._block(self,(features * 4) * 2, features * 4, name="dec3")
+        self.decoder3 = UNet2._block(self,(features * 4) * 2, features * 4, name="dec3")
         self.upconv2 = nn.ConvTranspose2d(
             features * 4, features * 2, kernel_size=2, stride=2
         )
-        self.decoder2 = UNet._block(self,(features * 2) * 2, features * 2, name="dec2")
+        self.decoder2 = UNet2._block(self,(features * 2) * 2, features * 2, name="dec2")
         self.upconv1 = nn.ConvTranspose2d(
             features * 2, features, kernel_size=2, stride=2
         )
-        self.decoder1 = UNet._block(self,features * 2, features, name="dec1")
+        self.decoder1 = UNet2._block(self,features * 2, features, name="dec1")
 
         self.conv = nn.Conv2d(
             in_channels=features, out_channels=out_channels, kernel_size=1
